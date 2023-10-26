ba9d00cda8ead7b1e9790c583f48a1a8939b82d1
lizalaveikina <116518356+lizalaveikina@users.noreply.github.com>
2023-10-26 22:21:04 +0300

    Update Item.java

--- src/main/java/ua/edu/ucu/apps/demo/flowers/Item.java
+++ src/main/java/ua/edu/ucu/apps/demo/flowers/Item.java
@@ -3,5 +3,5 @@ package ua.edu.ucu.apps.demo.flowers;
 public abstract class Item {
     public abstract double price();
 
-    public abstract String getDescription();
+    // public abstract String getDescription();
 }
