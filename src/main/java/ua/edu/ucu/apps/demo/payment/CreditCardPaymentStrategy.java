89dea5a41f453d0d8549a1ee44263d57b8378fc2
lizalaveikina <116518356+lizalaveikina@users.noreply.github.com>
2023-10-26 22:22:26 +0300

    Update CreditCardPaymentStrategy.java

--- src/main/java/ua/edu/ucu/apps/demo/payment/CreditCardPaymentStrategy.java
+++ src/main/java/ua/edu/ucu/apps/demo/payment/CreditCardPaymentStrategy.java
@@ -3,7 +3,8 @@ package ua.edu.ucu.apps.demo.payment;
 public class CreditCardPaymentStrategy implements Payment {
     @Override
     public double pay(double price) {
-        System.out.println("Credit Card method");
+        
+        System.out.println("Card");
         return price;
     }
 
